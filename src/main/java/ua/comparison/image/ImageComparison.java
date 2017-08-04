af3823ed545594d472a5e3c268d8f222f1cddb1a
rbeskrovnyi <rbeskrovnyi@sciforce.solutions>
2017-08-04 09:28:22 +0300

    add formatting and minor changes.

--- src/main/java/ua/comparison/image/ImageComparison.java
+++ src/main/java/ua/comparison/image/ImageComparison.java
@@ -10,8 +10,9 @@ public class ImageComparison {
 
     /**
      * The threshold which means the max distance between non-equal pixels.
+     * Could be changed according size and requirements to the image.
      */
-    public static int THRESHOLD = 5;
+    public static int threshold = 5;
 
     /**
      * The first number which marks first rectangle.
@@ -28,9 +29,7 @@ public class ImageComparison {
 
         // make dir if it's not using from Gradle.
         new File( "build" );
-        ImageIO.write( drawnDifferences,
-                "png",
-                new File( "build/result.png" ) );
+        ImageIO.write( drawnDifferences, "png", new File( "build/result.png" ) );
 
         ImageResultFrame.createGUI( drawnDifferences );
     }
@@ -41,9 +40,9 @@ public class ImageComparison {
      * @param image2 {@code BufferedImage} object of the second image.
      * @return the result of the drawing.
      */
-    public static BufferedImage drawTheDifference(BufferedImage image1, BufferedImage image2 ) {
+    public static BufferedImage drawTheDifference( BufferedImage image1, BufferedImage image2 ) {
         // check images for valid
-        checkingImages(image1,image2);
+        checkingImages( image1, image2 );
 
         int width = image1.getWidth();
         int height = image1.getHeight();
@@ -52,22 +51,22 @@ public class ImageComparison {
 
         BufferedImage outImg = new BufferedImage( width, height, BufferedImage.TYPE_INT_RGB );
 
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
+        for ( int y = 0; y < height; y++ ) {
+            for ( int x = 0; x < width; x++ ) {
                 if ( isDifferent( x, y, image1, image2 ) ) {
                     matrix[x][y] = 1;
                 } else {
                     matrix[x][y] = 0;
                 }
-                outImg.setRGB(x, y, image2.getRGB(x, y) );
+                outImg.setRGB( x, y, image2.getRGB( x, y ) );
             }
         }
 
         Graphics2D graphics = outImg.createGraphics();
         graphics.setColor( Color.RED );
 
-        int lastNumberCount = groupRegions(matrix, THRESHOLD);
-        drawRectangles(matrix, graphics, COUNTER, lastNumberCount );
+        int lastNumberCount = groupRegions( matrix, threshold );
+        drawRectangles( matrix, graphics, COUNTER, lastNumberCount );
         return outImg;
     }
 
@@ -77,12 +76,12 @@ public class ImageComparison {
      * @param threshold The threshold which means the max distance between non-equal pixels.
      * @return the last number which marks the lat region.
      */
-    private static int groupRegions(int[][] matrix, int threshold) {
+    private static int groupRegions( int[][] matrix, int threshold ) {
         int regionCount = 2;
-        for (int row = 0; row < matrix.length; row++) {
-            for (int col = 0; col < matrix[row].length; col++) {
-                if (matrix[row][col] == 1) {
-                    joinToRegion(matrix, row, col, regionCount, threshold);
+        for ( int row = 0; row < matrix.length; row++ ) {
+            for ( int col = 0; col < matrix[row].length; col++ ) {
+                if ( matrix[row][col] == 1 ) {
+                    joinToRegion( matrix, row, col, regionCount, threshold );
                     regionCount++;
                 }
             }
@@ -97,7 +96,7 @@ public class ImageComparison {
      * @param counter the number from marks regions.
      * @param lastNumberCount the last number which marks region.
      */
-    private static void drawRectangles(int[][] matrix, Graphics2D graphics, int counter, int lastNumberCount ) {
+    private static void drawRectangles( int[][] matrix, Graphics2D graphics, int counter, int lastNumberCount ) {
         int minX = Integer.MAX_VALUE;
         int minY = Integer.MAX_VALUE;
         int maxX = Integer.MIN_VALUE;
@@ -106,22 +105,22 @@ public class ImageComparison {
         if( counter > lastNumberCount ) return;
 
         boolean find = false;
-        for (int y = 0; y < matrix.length; y++) {
-            for (int x = 0; x < matrix[0].length; x++) {
-                if (matrix[y][x] == counter) {
+        for ( int y = 0; y < matrix.length; y++ ) {
+            for ( int x = 0; x < matrix[0].length; x++ ) {
+                if ( matrix[y][x] == counter ) {
                     find = true;
 
-                    if (x < minX) minX = x;
-                    if (x > maxX) maxX = x;
+                    if ( x < minX ) minX = x;
+                    if ( x > maxX ) maxX = x;
 
-                    if (y < minY) minY = y;
-                    if (y > maxY) maxY = y;
+                    if ( y < minY ) minY = y;
+                    if ( y > maxY ) maxY = y;
                 }
             }
         }
-        if (find) {
-            graphics.drawRect( minY, minX, maxY - minY, maxX - minX);
-            drawRectangles(matrix, graphics, ++counter, lastNumberCount );
+        if ( find ) {
+            graphics.drawRect( minY, minX, maxY - minY, maxX - minX );
+            drawRectangles( matrix, graphics, ++counter, lastNumberCount );
         }
     }
 
@@ -136,13 +135,13 @@ public class ImageComparison {
      * @param threshold the max distance between two values which equal "1".
      */
     private static void joinToRegion( int[][] matrix, int row, int col, int regionCount, int threshold ) {
-        if (row < 0 || row >= matrix.length) return;
-        if (col < 0 || col >= matrix[row].length) return;
-        if (matrix[row][col] != 1) return;
+        if ( row < 0 || row >= matrix.length ) return;
+        if ( col < 0 || col >= matrix[row].length ) return;
+        if ( matrix[row][col] != 1 ) return;
 
         matrix[row][col] = regionCount;
 
-        for (int i = 0; i < threshold; i++) {
+        for ( int i = 0; i < threshold; i++ ) {
             // goes to all directions.
             joinToRegion(matrix, row - 1 - i, col, regionCount, threshold);
             joinToRegion(matrix, row + 1 + i, col, regionCount, threshold);
@@ -154,7 +153,6 @@ public class ImageComparison {
             joinToRegion(matrix, row - 1 - i, col + 1 + i, regionCount, threshold);
             joinToRegion(matrix, row + 1 + i, col + 1 + i, regionCount, threshold);
         }
-
     }
 
     /**
@@ -166,19 +164,19 @@ public class ImageComparison {
      * @param image2 {@code BufferedImage} object of the second image.
      * @return {@code true} if they' are difference, {@code false} otherwise.
      */
-    private static boolean isDifferent(int x, int y, BufferedImage image1, BufferedImage image2){
+    private static boolean isDifferent( int x, int y, BufferedImage image1, BufferedImage image2 ){
         boolean result = false;
-        int[] im1= image1.getRaster().getPixel(x,y,new int[3]);
-        int[] im2= image2.getRaster().getPixel(x,y,new int[3]);
+        int[] im1= image1.getRaster().getPixel( x,y,new int[3] );
+        int[] im2= image2.getRaster().getPixel( x,y,new int[3] );
         //gets modules of the images:
-        double mod1 = Math.sqrt(im1[0]*im1[0]+im1[1]*im1[1]+im1[2]*im1[2]);
-        double mod2 = Math.sqrt(im2[0]*im2[0]+im2[1]*im2[1]+im2[2]*im2[2]);
+        double mod1 = Math.sqrt( im1[0] * im1[0] + im1[1] * im1[1] + im1[2] * im1[2] );
+        double mod2 = Math.sqrt( im2[0] * im2[0] + im2[1] * im2[1] + im2[2] * im2[2] );
         // gets module of the difference of images.
-        double mod3 = Math.sqrt( Math.abs(im1[0]-im2[0]) * Math.abs(im1[0]-im2[0]) +
-                Math.abs(im1[1]-im2[1]) * Math.abs(im1[1]-im2[1]) +
-                Math.abs(im1[2]-im2[2])*Math.abs(im1[2]-im2[2]));
-        double imageChanges1 = mod3/mod1;
-        double imageChanges2 = mod3/mod2;
+        double mod3 = Math.sqrt( Math.abs( im1[0] - im2[0] ) * Math.abs( im1[0] - im2[0] ) +
+                Math.abs( im1[1] - im2[1] ) * Math.abs( im1[1] - im2[1] ) +
+                Math.abs( im1[2] - im2[2] ) * Math.abs( im1[2] - im2[2] ) );
+        double imageChanges1 = mod3 / mod1;
+        double imageChanges2 = mod3 / mod2;
         if( imageChanges1 > 0.1 && imageChanges2 > 0.1 ) result = true;
         return result;
     }
@@ -190,8 +188,8 @@ public class ImageComparison {
      * @throws IOException
      * @throws URISyntaxException
      */
-    static BufferedImage readImageFromResources(String path) throws IOException, URISyntaxException {
-        return ImageIO.read( new File( ImageComparison.class.getClassLoader().getResource (path ).toURI().getPath()) );
+    private static BufferedImage readImageFromResources( String path ) throws IOException, URISyntaxException {
+        return ImageIO.read( new File( ImageComparison.class.getClassLoader().getResource ( path ).toURI().getPath() ) );
     }
 
     /**
@@ -200,7 +198,7 @@ public class ImageComparison {
      * @param image2 {@code BufferedImage} object of the second image.
      */
     private static void checkingImages( BufferedImage image1, BufferedImage image2 ) {
-        if( image1.getWidth() != image2.getWidth() || image2.getHeight() != image2.getHeight())
-            throw new IllegalArgumentException("Images dimensions mismatch");
+        if( image1.getWidth() != image2.getWidth() || image2.getHeight() != image2.getHeight() )
+            throw new IllegalArgumentException( "Images dimensions mismatch" );
     }
 }
