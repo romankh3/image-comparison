c396fac4318523bddbf58031cbf5d1968058644b
Artem Kudrya <ak98nneon@gmail.com>
2019-05-11 16:01:03 +0300

    Feature/improvement move main from image comparison 73 (#77)
    #73: [IMPROVEMENT] Move main(String[] args) from ImageComparison.

---
+++ src/main/java/com/github/romankh3/image/comparison/Main.java
@@ -0,0 +1,18 @@
+package com.github.romankh3.image.comparison;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+/**
+ * @author Artem Kudria
+ * Class for the presentation of the program
+ */
+public class Main {
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        ImageComparison imgCmp = CommandLineUtil.create(args);
+        BufferedImage result = imgCmp.compareImages();
+        CommandLineUtil.handleResult(imgCmp, (file) -> ImageComparisonTools.saveImage(file, result), () -> ImageComparisonTools
+                .createGUI(result));
+    }
+}
--- src/test/java/com/github/romankh3/image/comparison/ImageComparisonUnitTest.java
+++ src/test/java/com/github/romankh3/image/comparison/ImageComparisonUnitTest.java
@@ -37,23 +37,6 @@ public class ImageComparisonUnitTest {
         }
     }
 
-    @Test
-    public void testMainClass() throws IOException, URISyntaxException {
-        //given
-        String[] args = new String[2];
-        File image1 = new File(ImageComparison.class.getClassLoader().getResource("image1.png").toURI().getPath());
-        File image2 = new File(ImageComparison.class.getClassLoader().getResource("image2.png").toURI().getPath());
-        args[0] = image1.getAbsolutePath();
-        args[1] = image2.getAbsolutePath();
-
-        //when
-        ImageComparison.main(args);
-
-        //then
-        assertNotNull(image1);
-        assertNotNull(image2);
-    }
-
     /**
      * The most important test. Shown, that the changes in algorithm,
      * don't break the main behaviour and result as expected.
