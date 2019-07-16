fc1900837982dcc7b6e6afd4a5a4fc92f8a55b83
romankh3 <roman.beskrovnyy@gmail.com>
2019-07-16 13:51:10 +0300

    Javadoc update.

--- src/main/java/com/github/romankh3/image/comparison/model/ExcludedAreas.java
+++ src/main/java/com/github/romankh3/image/comparison/model/ExcludedAreas.java
@@ -8,16 +8,35 @@ import java.util.List;
  */
 public class ExcludedAreas {
 
+    /**
+     * The collection of the areas which would be excluded from the comparison.
+     */
     private final List<Rectangle> excluded;
 
+    /**
+     * Create empty instance of the {@link ExcludedAreas}.
+     */
     public ExcludedAreas() {
         excluded = new ArrayList<>();
     }
 
+    /**
+     * Create instance of the {@link ExcludedAreas} with provided {@link Rectangle} areas.
+     *
+     * @param excluded provided collection of the {@link Rectangle} objects.
+     */
     public ExcludedAreas(List<Rectangle> excluded) {
         this.excluded = excluded;
     }
 
+    /**
+     * Check if this {@link Point} contains in the {@link ExcludedAreas#excluded}
+     * collection of the {@link Rectangle}.
+     *
+     * @param point the {@link Point} object to be checked.
+     *
+     * @return {@code true} if this {@link Point} contains in areas from {@link ExcludedAreas#excluded}.
+     */
     public boolean contains(Point point) {
         return excluded.stream().anyMatch(rectangle -> rectangle.containsPoint(point));
     }
