b61caab1b199f0a092efdbeb7f95b386298fb2e2
Bon-Bon <1157264534@qq.com>
2022-05-24 14:55:04 +0800

    Add functions for issue#190

--- src/main/java/com/github/romankh3/image/comparison/ImageComparisonUtil.java
+++ src/main/java/com/github/romankh3/image/comparison/ImageComparisonUtil.java
@@ -142,11 +142,12 @@ public final class ImageComparisonUtil {
         long diff = 0;
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                diff += pixelDiff(img1.getRGB(x, y), img2.getRGB(x, y));
+                if(isDiffPixel(img1.getRGB(x, y), img2.getRGB(x, y))) {
+                    diff += 1;
+                }
             }
         }
-        long maxDiff = 3L * 255 * width * height;
-
+        long maxDiff = (long) width * height;
         return (float) (100.0 * diff / maxDiff);
     }
 
@@ -165,5 +166,26 @@ public final class ImageComparisonUtil {
         int g2 = (rgb2 >> 8) & 0xff;
         int b2 = rgb2 & 0xff;
         return Math.abs(r1 - r2) + Math.abs(g1 - g2) + Math.abs(b1 - b2);
+    }
+
+
+    /**
+     * Compare two pixels
+     *
+     * @param rgb1 the first rgb
+     * @param rgb2 the second rgn
+     * @return true If they are the same RGB pixel.
+     */
+    public static boolean isDiffPixel(int rgb1, int rgb2) {
+        int r1 = (rgb1 >> 16) & 0xff;
+        int g1 = (rgb1 >> 8) & 0xff;
+        int b1 = rgb1 & 0xff;
+        int r2 = (rgb2 >> 16) & 0xff;
+        int g2 = (rgb2 >> 8) & 0xff;
+        int b2 = rgb2 & 0xff;
+        if(Math.abs(r1 - r2) + Math.abs(g1 - g2) + Math.abs(b1 - b2) == 0) {
+            return false;
+        }
+        return true;
     }
 }
